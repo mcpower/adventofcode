36d2987b4de6b811ece46bcbf91c9aa39078059c
Michael <mcpower@users.noreply.github.com>
2017-12-08 16:09:22 +1100

    2017 Day 8

---
+++ 2017/08/a-old.py
@@ -0,0 +1,42 @@
+#!/usr/bin/pypy3
+from itertools import *
+import math
+
+USE_FILE = 1
+if not USE_FILE:
+    inp = r"""
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+""".strip()
+    inp = r"""
+
+""".strip() or inp
+    inp = r"""
+
+""".strip() or inp
+    inp = r"""
+
+""".strip() or inp
+    inp = r"""
+
+""".strip() or inp
+else:
+    inp = open("input.txt").read().strip()
+
+assert isinstance(inp, str)
+
+lines = inp.splitlines()
+
+reg = {}
+# default 0
+
+for line in lines:
+    var, ty, num, _, var2, op, cond = line.split()
+
+    if eval("reg.get('{}',0) {} {}".format(var2, op, cond)):
+        reg[var] = reg.get(var, 0) + (1 if ty == "inc" else -1) * int(num)
+    
+
+print(max(reg.values()))
