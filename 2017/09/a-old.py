57ec8a5e9e7cafaceee061674eefbce96111fe0b
Michael <mcpower@users.noreply.github.com>
2017-12-09 16:07:39 +1100

    2017 Day 9

---
+++ 2017/09/a-old.py
@@ -0,0 +1,57 @@
+#!/usr/bin/pypy3
+from itertools import *
+import math
+
+USE_FILE = 1
+if not USE_FILE:
+    inp = r"""
+{{<!!>},{<!!>},{<!!>},{<!!>}}
+""".strip()
+    inp = r"""
+{{<a!>},{<a!>},{<a!>},{<ab>}}
+""".strip() or inp
+    inp = r"""
+{{<ab>},{<ab>},{<ab>},{<ab>}}
+""".strip() or inp
+    inp = r"""
+
+""".strip() or inp
+else:
+    inp = open("input.txt").read().strip()
+
+assert isinstance(inp, str)
+
+lmap = lambda func, *iterables: list(map(func, *iterables))
+splitf = lambda s, f=int: lmap(f,s.split())
+
+
+
+level = 0
+is_garbage = False
+ignore = False
+
+out = 0
+
+for char in inp:
+    if is_garbage:
+        if ignore:
+            ignore = False
+            continue
+        if char == '>':
+            is_garbage = False
+        if char == '!':
+            ignore = True
+            continue
+        continue
+    
+    if char == '{':
+        level += 1
+        continue
+    if char == '}':
+        out += level
+        level -= 1
+    if char == '<':
+        is_garbage = True
+
+assert(level == 0)
+print(out)
