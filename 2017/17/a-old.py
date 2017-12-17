829ab5aec768226039e7bbc6933e56e248ebde5d
Michael <mcpower@users.noreply.github.com>
2017-12-17 16:09:18 +1100

    2017 Day 17

---
+++ 2017/17/a-old.py
@@ -0,0 +1,97 @@
+#!/usr/bin/pypy3
+from pprint import pprint
+from functools import reduce
+import operator
+import sys
+sys.setrecursionlimit(100000)
+lmap = lambda func, *iterables: list(map(func, *iterables))
+splitf = lambda s, f=int: lmap(f,s.split())
+def d(*a, **k):
+    if DEBUG:
+        print(*a, **k)
+
+def dd(object):
+    if DEBUG:
+        pprint(object, compact=True)
+
+
+DEBUG = 1
+
+def knot(inp: str, binary = False) -> str:
+    lengths = lmap(ord, inp) + [17, 31, 73, 47, 23]
+    pos = skip = 0
+    l = list(range(256))
+    for _ in range(64):
+        for i in lengths:
+            positions = [x & 255 for x in range(pos, pos+i)]
+            oldl = list(l)
+            for x, y in zip(positions, reversed(positions)):
+                l[x] = oldl[y]
+            pos += i + skip
+            skip += 1
+    sparse = [reduce(operator.xor, l[i:i+16]) for i in range(0, 256, 16)]
+    if binary:
+        return "".join(bin(i)[2:].zfill(8) for i in sparse)
+    else:
+        return "".join(hex(i)[2:].zfill(2) for i in sparse)
+
+
+def do_case(inp: str, sample = False):
+    sprint = lambda *a, **k: sample and print(*a, **k)
+    
+    steps = int(inp)
+
+    lock = [0]
+
+    pos = 0
+
+    for i in range(2017):
+        new = (pos + steps) % len(lock)
+        new += 1
+        lock.insert(new, i+1)
+        pos = new
+    print(lock[pos+1])
+
+
+
+    return
+
+
+samples = [
+r"""
+3
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""
+]
+samples = lmap(str.strip, samples)
+while samples and not samples[-1]:
+    samples.pop()
+
+for sample in samples:
+    print("running {}:".format(repr(sample)[:100]))
+    print("-"*10)
+    do_case(sample, True)
+    print("-"*10)
+    print("#"*10)
+
+try:
+    actual_input = open("input.txt").read().strip()
+except FileNotFoundError:
+    actual_input = ""
+
+if actual_input:
+    print("!! running actual: !!")
+    print("-"*10)
+    do_case(actual_input, False)
+    print("-"*10)
