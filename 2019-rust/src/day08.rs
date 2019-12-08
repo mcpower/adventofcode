108bd391958a9cb270c7c888f55733d6f54ecf37
mcpower <mcpower@users.noreply.github.com>
2019-12-08 16:47:53 +1100

    2019 Day 8 - refactor with .chunks

--- 2019-rust/src/day08.rs
+++ 2019-rust/src/day08.rs
@@ -5,31 +5,19 @@ pub fn part1(inp: &str) -> String {
 
 fn _part1(inp: &str, _sample: bool) -> String {
     let chars: Vec<_> = inp.chars().collect();
-    let width = if _sample {
-        3
-    } else {
-        25
-    };
-    let height = if _sample {
-        2
-    } else {
-        6
-    };
-    let len = inp.len();
+    let width = if _sample { 3 } else { 25 };
+    let height = if _sample { 2 } else { 6 };
 
-    let layers = len / (width * height);
-    let mut fewest_digits = 100;
-    let mut out = 0;
-    for layer in 0..layers {
-        let v = &chars[layer*width*height..(layer+1)*width*height];
-        let zeroes = v.iter().filter(|x| **x == '0').count();
-        if zeroes < fewest_digits {
-            fewest_digits = zeroes;
-            out = v.iter().filter(|x| **x == '1').count() * v.iter().filter(|x| **x == '2').count()
-        }
+    let mut out = (100, 0);
+    for layer in chars.chunks(width * height) {
+        let get_num = |c| layer.iter().filter(|&&x| x == c).count();
+        let zeroes = get_num('0');
+        let ones = get_num('1');
+        let twos = get_num('2');
+        out = out.min((zeroes, ones * twos));
     }
 
-    out.to_string()
+    out.1.to_string()
 }
 
 #[aoc(day08, part2)]
@@ -39,52 +27,31 @@ pub fn part2(inp: &str) -> String {
 
 fn _part2(inp: &str, _sample: bool) -> String {
     let chars: Vec<_> = inp.chars().collect();
-    let width = if _sample {
-        3
-    } else {
-        25
-    };
-    let height = if _sample {
-        2
-    } else {
-        6
-    };
-    let len = inp.len();
+    let width = if _sample { 2 } else { 25 };
+    let height = if _sample { 2 } else { 6 };
+    let layers: Vec<_> = chars.chunks(width * height).collect();
 
-    let get_layer = |i: usize| {
-        let mut out = vec![];
-        let layers = len / (width * height);
-        for layer in 0..layers {
-            out.push(chars.get(layer*width*height + i).unwrap());
-        }
-        out
-    };
+    let mut output: String = String::new();
 
-    let mut out = vec![];
-
-    for i in 0..(width*height) {
-        let v = get_layer(i);
-        let q = v.iter().filter(|x| ***x != '2').next().unwrap();
-        out.push(**q);
-    }
-    let mut output = String::new();
-    output.push('\n');
     for row in 0..height {
-        for col in 0..width {
-            output.push(out[row*width + col]);
-        }
         output.push('\n');
+        for col in 0..width {
+            let pixel = layers
+                .iter()
+                .map(|layer| layer[row * width + col])
+                .find(|&c| c != '2')
+                .unwrap();
+            let ascii = if pixel == '1' { '#' } else { '.' };
+            output.push(ascii);
+        }
     }
-    output = output.replace('0', ".").replace('1', "#");
 
     output
 }
 
 #[test]
 fn day08samples() {
-//assert_eq!(_part1(r#"
-//"#.trim_matches('\n'), true), "");
+    assert_eq!(_part1("123456789012", true), "1");
 
-//assert_eq!(_part2(r#"
-//"#.trim_matches('\n'), true), "");
+    assert_eq!(_part2("0222112222120000", true).trim(), ".#\n#.");
 }
