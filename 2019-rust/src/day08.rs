aa61477b3cab2406890141353c8585c5f32684b7
mcpower <mcpower@users.noreply.github.com>
2019-12-08 17:24:35 +1100

    2019 Day 8 - major functional refactor

--- 2019-rust/src/day08.rs
+++ 2019-rust/src/day08.rs
@@ -3,50 +3,49 @@ pub fn part1(inp: &str) -> String {
     _part1(inp, false)
 }
 
-fn _part1(inp: &str, _sample: bool) -> String {
+fn _part1(inp: &str, sample: bool) -> String {
     let chars: Vec<_> = inp.chars().collect();
-    let width = if _sample { 3 } else { 25 };
-    let height = if _sample { 2 } else { 6 };
+    let cols = if sample { 3 } else { 25 };
+    let rows = if sample { 2 } else { 6 };
 
-    let mut out = (100, 0);
-    for layer in chars.chunks(width * height) {
-        let get_num = |c| layer.iter().filter(|&&x| x == c).count();
-        let zeroes = get_num('0');
-        let ones = get_num('1');
-        let twos = get_num('2');
-        out = out.min((zeroes, ones * twos));
-    }
-
-    out.1.to_string()
+    chars
+        .chunks(cols * rows)
+        .map(|layer| {
+            let num = |c| layer.iter().filter(|&&x| x == c).count();
+            (num('0'), num('1') * num('2'))
+        })
+        .min()
+        .unwrap()
+        .1
+        .to_string()
 }
 
 #[aoc(day08, part2)]
 pub fn part2(inp: &str) -> String {
-    _part2(inp, false)
+    // need extra new line because cargo-aoc displays the string inline with the day/part
+    "\n".to_string() + _part2(inp, false).as_ref()
 }
 
-fn _part2(inp: &str, _sample: bool) -> String {
+fn _part2(inp: &str, sample: bool) -> String {
     let chars: Vec<_> = inp.chars().collect();
-    let width = if _sample { 2 } else { 25 };
-    let height = if _sample { 2 } else { 6 };
-    let layers: Vec<_> = chars.chunks(width * height).collect();
+    let cols = if sample { 2 } else { 25 };
+    let rows = if sample { 2 } else { 6 };
+    let layers: Vec<_> = chars.chunks(cols * rows).collect();
 
-    let mut output: String = String::new();
-
-    for row in 0..height {
-        output.push('\n');
-        for col in 0..width {
-            let pixel = layers
-                .iter()
-                .map(|layer| layer[row * width + col])
-                .find(|&c| c != '2')
-                .unwrap();
-            let ascii = if pixel == '1' { '#' } else { '.' };
-            output.push(ascii);
-        }
-    }
-
-    output
+    (0..rows)
+        .map(|row| {
+            (0..cols)
+                .filter_map(|col| {
+                    layers
+                        .iter()
+                        .map(|layer| layer[row * cols + col])
+                        .find(|&c| c != '2')
+                })
+                .map(|pixel| if pixel == '1' { '#' } else { '.' })
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 #[test]
