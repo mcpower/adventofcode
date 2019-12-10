d0068b360960136b5725c619ed13bb2213ae699f
mcpower <mcpower@users.noreply.github.com>
2019-12-10 17:55:00 +1100

    2019 Day 10 - cleanup

--- 2019-rust/src/day10.rs
+++ 2019-rust/src/day10.rs
@@ -1,12 +1,13 @@
+#[allow(clippy::many_single_char_names)]
 fn farey(n: usize) -> Vec<(usize, usize)> {
-    let (mut a,mut b,mut c,mut d) = (0,1,1,n);
+    let (mut a, mut b, mut c, mut d) = (0, 1, 1, n);
     let mut out = vec![];
     out.push((a, b));
 
     while c < n {
-        let k = (n+b) / d;
+        let k = (n + b) / d;
 
-        let (newa,newb,newc,newd) = (c, d, k * c - a, k * d - b);
+        let (newa, newb, newc, newd) = (c, d, k * c - a, k * d - b);
         a = newa;
         b = newb;
         c = newc;
@@ -33,65 +34,50 @@ pub fn part1(inp: &str) -> String {
 }
 
 fn _part1(inp: &str, _sample: bool) -> String {
-    let grid: Vec<Vec<bool>> = inp.lines().map(|line|
-        line.chars().map(|c| c == '#').collect()
-    ).collect();
-
-    let mut edges: Vec<Vec<Vec<(usize, usize)>>> = grid.iter().map(|row| row.iter().map(|x| vec![]).collect()).collect();
+    let grid: Vec<Vec<bool>> = inp
+        .lines()
+        .map(|line| line.chars().map(|c| c == '#').collect())
+        .collect();
 
     let rows = grid.len();
     let cols = grid[0].len();
 
-//    dbg!(gcd(-6, 1));
-
+    let mut best = 0;
     for row in 0..rows {
         for col in 0..cols {
             if !grid[row][col] {
                 continue;
             }
+            let mut count = 0;
             for other_row in 0..rows {
                 for other_col in 0..cols {
-                    if (other_row, other_col) == (row, col) {
-                        continue;
-                    }
                     let drow = (other_row as i64) - (row as i64);
                     let dcol = (other_col as i64) - (col as i64);
-                    if drow == 0 && i64::abs(dcol) != 1 {
-                        continue;
-                    }
-                    if dcol == 0 && i64::abs(drow) != 1 {
-                        continue;
-                    }
+                    // if (drow, dcol) = (0, 0) this should be false
                     if gcd(drow, dcol) != 1 {
                         continue;
                     }
                     let mut cur_row = other_row as i64;
                     let mut cur_col = other_col as i64;
-//                    if (row, col) == (4, 3) {
-//                        dbg!(other_row, other_col, drow, dcol);
-//                    }
-                    while 0 <= cur_row && cur_row < rows as i64 && 0 <= cur_col && cur_col < cols as i64 {
-                        if grid[cur_row as usize][cur_col as usize] {
-                            edges[row][col].push((cur_row as usize, cur_col as usize));
-                            break;
+                    while 0 <= cur_row
+                        && cur_row < rows as i64
+                        && 0 <= cur_col
+                        && cur_col < cols as i64
+                        {
+                            if grid[cur_row as usize][cur_col as usize] {
+                                count += 1;
+                                break;
+                            }
+                            cur_row += drow;
+                            cur_col += dcol;
                         }
-                        cur_row += drow;
-                        cur_col += dcol;
-                    }
                 }
             }
+            best = best.max(count);
         }
     }
 
-//    dbg!(edges.iter().map(|row| row.iter().map(|thing| thing.len()).collect::<Vec<_>>()).collect::<Vec<_>>());
-
-    edges.iter()
-        .flat_map(|row|
-            row.iter().map(|thing| thing.len())
-        )
-        .max()
-        .unwrap()
-        .to_string()
+    best.to_string()
 }
 
 #[aoc(day10, part2)]
@@ -100,46 +86,38 @@ pub fn part2(inp: &str) -> String {
 }
 
 fn _part2(inp: &str, _sample: bool) -> String {
-    let mut grid: Vec<Vec<bool>> = inp.lines().map(|line|
-        line.chars().map(|c| c == '#').collect()
-    ).collect();
-
-    let mut edges: Vec<Vec<usize>> = grid.iter().map(|row| row.iter().map(|x| 0).collect()).collect();
+    let grid: Vec<Vec<bool>> = inp
+        .lines()
+        .map(|line| line.chars().map(|c| c == '#').collect())
+        .collect();
 
     let rows = grid.len();
     let cols = grid[0].len();
 
-//    dbg!(gcd(-6, 1));
-
+    let mut best = (0usize, 0usize, 0usize);
     for row in 0..rows {
         for col in 0..cols {
             if !grid[row][col] {
                 continue;
             }
+            let mut count = 0;
             for other_row in 0..rows {
                 for other_col in 0..cols {
-                    if (other_row, other_col) == (row, col) {
-                        continue;
-                    }
                     let drow = (other_row as i64) - (row as i64);
                     let dcol = (other_col as i64) - (col as i64);
-                    if drow == 0 && i64::abs(dcol) != 1 {
-                        continue;
-                    }
-                    if dcol == 0 && i64::abs(drow) != 1 {
-                        continue;
-                    }
+                    // if (drow, dcol) = (0, 0) this should be false
                     if gcd(drow, dcol) != 1 {
                         continue;
                     }
                     let mut cur_row = other_row as i64;
                     let mut cur_col = other_col as i64;
-//                    if (row, col) == (4, 3) {
-//                        dbg!(other_row, other_col, drow, dcol);
-//                    }
-                    while 0 <= cur_row && cur_row < rows as i64 && 0 <= cur_col && cur_col < cols as i64 {
+                    while 0 <= cur_row
+                        && cur_row < rows as i64
+                        && 0 <= cur_col
+                        && cur_col < cols as i64
+                    {
                         if grid[cur_row as usize][cur_col as usize] {
-                            edges[row][col] += 1;
+                            count += 1;
                             break;
                         }
                         cur_row += drow;
@@ -147,20 +125,12 @@ fn _part2(inp: &str, _sample: bool) -> String {
                     }
                 }
             }
-        }
-    }
-
-    let mut best = (0usize, 0usize, 0usize);
-
-    for row in 0..rows {
-        for col in 0..cols {
-            best = best.max((edges[row][col], row, col));
+            best = best.max((count, row, col));
         }
     }
 
     let best_row = best.1 as i64;
     let best_col = best.2 as i64;
-    let mut destoyed = 0;
 
     // (0, 1) to (1, 0)
     let mut seq = farey(rows.max(cols) + 1);
@@ -177,12 +147,7 @@ fn _part2(inp: &str, _sample: bool) -> String {
     wtf.extend(seq.iter().map(|(x, y)| (-(*x as i64), -(*y as i64))));
     wtf.pop();
 
-//    dbg!(wtf);
-//    return "".to_string();
-
-    let mut todo: Vec<Vec<i64>>= vec![];
-
-
+    let mut todo: Vec<Vec<i64>> = vec![];
 
     for (drow, dcol) in &wtf {
         let drow = *drow;
@@ -203,14 +168,12 @@ fn _part2(inp: &str, _sample: bool) -> String {
         }
     }
 
-//    dbg!(todo.iter().flatten().collect::<Vec<_>>());
-
     todo.iter().flatten().take(200).last().unwrap().to_string()
 }
 
 #[test]
 fn day10samples() {
-assert_eq!(_part1(r#"
+    assert_eq!(_part1(r#"
 .#..#
 .....
 #####
