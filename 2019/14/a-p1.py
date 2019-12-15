7fa794a5b46830c8d466cdb75dbb1813801cf453
Michael <mcpower@users.noreply.github.com>
2019-12-15 14:53:38 +1100

    2019 Day 14 (Python)

---
+++ 2019/14/a-p1.py
@@ -0,0 +1,105 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words
+
+Data structures:
+Linked, UnionFind
+dict: d.keys(), d.values(), d.items()
+deque: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+    recipes = {}
+    for line in lines:
+        l,r = line.split(" => ")
+        n,i = r.split()
+        recipes[i] = [int(n), [(int(s.split()[0]), s.split()[1]) for s in l.split(",")]]
+    
+    out_edges = {
+        k: [y for x,y in v[1]]
+        for k, v in recipes.items()
+    }
+    wanted = defaultdict(int)
+    wanted["FUEL"] = 1
+    topsorted = topsort(out_edges) 
+    for ing in topsorted:
+        if ing == "ORE":
+            break
+        n = wanted[ing]
+        p, ings = recipes[ing]
+        r = (n + p - 1) // p
+        for needed, other_ing in ings:
+            wanted[other_ing] += r * needed
+    sprint(topsorted)
+    print(wanted["ORE"])
+    
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL
+""",r"""
+171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""],[
+# Part 2
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
