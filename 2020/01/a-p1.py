3a92c15a1c4a5cbc8442e201bb25e159c9e4dd0d
Michael <mcpower@users.noreply.github.com>
2020-12-01 18:38:13 +1100

    2020 Day 1

---
+++ 2020/01/a-p1.py
@@ -0,0 +1,66 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words
+
+Data structures:
+Linked, UnionFind
+dict: d.keys(), d.values(), d.items()
+deque: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+    q = ints(inp)
+    for a in q:
+        for b in q:
+            if a + b == 2020:
+                print(a*b)
+    
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""],[
+# Part 2
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
