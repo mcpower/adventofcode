421022ba433cf7b28b4b8dcb0d151ef95ce3aa23
Michael <mcpower@users.noreply.github.com>
2020-12-06 16:17:49 +1100

    2020 Day 6 + runner change

---
+++ 2020/06/a-p1.py
@@ -0,0 +1,83 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words
+
+Data structures:
+Linked, UnionFind
+dict: d.keys(), d.values(), d.items()
+deque: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+    out = 0
+
+    groups = inp.split("\n\n")
+    sprint(groups)
+    print(sum(len(set(group) - set(" \n")) for group in groups))
+
+    
+    if out:
+        print("out:    ", out)
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""],[
+# Part 2
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
