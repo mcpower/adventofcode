448854394943d1d883e7986545fd39c612d9db68
Michael <mcpower@users.noreply.github.com>
2020-12-06 16:18:32 +1100

    Add paras to template

--- a.py
+++ a.py
@@ -21,6 +21,7 @@ def do_case(inp: str, sample=False):
     # READ THE PROBLEM FROM TOP TO BOTTOM OK
     def sprint(*a, **k): sample and print(*a, **k)
     lines = inp.splitlines()
+    paras = inp.split("\n\n")
     out = 0
     
     if out:
