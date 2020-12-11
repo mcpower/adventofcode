2a51edd72df97a4708119989a298a1c790335f50
Michael <mcpower@users.noreply.github.com>
2020-12-11 16:14:16 +1100

    2020 Day 11

---
+++ 2020/11/a-p2.py
@@ -0,0 +1,115 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words
+
+Data structures:
+Linked, UnionFind
+dict: d.keys(), d.values(), d.items()
+deque: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+"""
+
+
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+    paras = inp.split("\n\n")
+    out = 0
+
+    grid = lmap(list, lines)
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+
+    def next_grid(grid):
+        new = copy.deepcopy(grid)
+
+        for row in range(rows):
+            for col in range(cols):
+                cur = grid[row][col]
+                neighbours = []
+
+                for drow, dcol in OCT_DELTA:
+                    for i in range(1, 10000):
+                        if 0 <= row+drow*i < rows and 0 <= col+dcol*i < cols:
+                            new_thing = grid[row+drow*i][col+dcol*i]
+                            if new_thing != ".":
+                                neighbours.append(new_thing)
+                                break
+                        else:
+                            break
+                if cur == "L" and neighbours.count("#") == 0:
+                    new[row][col] = "#"
+                
+                if cur == "#" and neighbours.count("#") >= 5:
+                    new[row][col] = "L"
+        
+        return new
+    
+    new_grid = next_grid(grid)
+    while new_grid != grid:
+        grid, new_grid = new_grid, next_grid(new_grid)
+        # out += 1
+    
+    print(flatten(grid).count("#"))
+    
+    if out:
+        print("out:    ", out)
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""],[
+# Part 2
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
