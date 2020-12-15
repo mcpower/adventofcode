9950fbeed8ed60d69a75023d10a68040e0c29f73
Michael <mcpower@users.noreply.github.com>
2020-12-15 16:15:32 +1100

    2020 Day 15

---
+++ 2020/15/a-p1.py
@@ -0,0 +1,48 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+    paras = inp.split("\n\n")
+    out = 0
+
+    nums = ints(inp)
+
+    last_seen = {x: i for i, x in enumerate(nums[:-1])}
+
+    while len(nums) != 2020:
+        last = nums[-1]
+        if last in last_seen:
+            this = last_seen[nums[-1]]
+            if this == len(nums) - 1:
+                nums.append(0)
+            else:
+                nums.append(len(nums) - this - 1)
+        else:
+            nums.append(0)
+        last_seen[nums[-2]] = len(nums) - 2
+    print(nums[:10])
+    print(nums[2019])
+    if out:
+        print("out:    ", out)
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+r"""
+0,3,6
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
