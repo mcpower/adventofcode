9ac624e21bd3ddeefbb01f34c302a5eabe1d6fc1
mcpower <mcpower@users.noreply.github.com>
2020-12-24 16:15:48 +1100

    Update template

--- a.py
+++ a.py
@@ -4,7 +4,7 @@ def do_case(inp: str, sample=False):
     # READ THE PROBLEM FROM TOP TO BOTTOM OK
     def sprint(*a, **k): sample and print(*a, **k)
     lines = inp.splitlines()
-    paras = inp.split("\n\n")
+    paras = lmap(str.splitlines, inp.split("\n\n"))
     out = 0
     
     if out:
