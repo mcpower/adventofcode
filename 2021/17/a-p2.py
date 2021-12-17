f7199b7cbb74f467b1cbe2ba4520cec6748e09fb
mcpower <mcpower@users.noreply.github.com>
2021-12-17 16:10:35 +1100

    2021 Day 17

---
+++ 2021/17/a-p2.py
@@ -0,0 +1,61 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines: typing.List[str] = inp.splitlines()
+    paras: typing.List[typing.List[str]] = lmap(str.splitlines, inp.split("\n\n"))
+    out = 0
+
+    minx,  maxx , miny, maxy = ints(inp)
+
+    def height(vx, vy) -> typing.Optional[int]:
+        x, y = 0, 0
+        # from future mcpower: this is slooow. inline this for performance!
+        def step():
+            nonlocal vx, vy, x, y
+            x += vx
+            y += vy
+            vy -= 1 
+            vx -= signum(vx)
+        
+        highest = 0
+        while True:
+            highest = max(highest, y)
+            if minx <= x <= maxx and miny <= y <= maxy:
+                return highest
+            if y < miny and vy < 0:
+                return None
+            if x > maxx:
+                return None
+            step()
+        # return None
+
+    for vx in range(-3, 1000):
+        for vy in range(-200, 2000):
+            asdf = height(vx, vy)
+            if asdf is not None:
+                out += 1
+    
+    if out:
+        print("out:    ", out)
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+r"""
+target area: x=20..30, y=-10..-5
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
