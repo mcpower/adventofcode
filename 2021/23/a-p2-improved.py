65f868705baf7828eee279e701174863322a7247
mcpower <mcpower@users.noreply.github.com>
2021-12-23 17:39:24 +1100

    More refactoring of improved 2021 Day 23

--- 2021/23/a-p2-improved.py
+++ 2021/23/a-p2-improved.py
@@ -1,10 +1,5 @@
 import sys; sys.dont_write_bytecode = True; from utils import *
 
-WAITING_AREAS = [
-    (1, x)
-    for x in [1, 2, 4, 6, 8, 10, 11]
-]
-
 WAITING_ROW = 1
 WAITING_COLS = [1, 2, 4, 6, 8, 10, 11]
 COST = {"A": 1, "B": 10, "C": 100, "D": 1000}
@@ -25,7 +20,8 @@ def do_case(inp: str, sample=False):
         # (weight, node)
         out = []
 
-        # node should store waiting areas + rooms
+        # I wrote this line of code first, before doing anything else
+        # with the problem.
         cur_waitings, cur_rooms = node
 
         if all(all(chr(ord('A')+i) == x for x in room) for i, room in enumerate(cur_rooms)):
@@ -35,12 +31,13 @@ def do_case(inp: str, sample=False):
             if col_1 > col_2:
                 col_1, col_2 = col_2, col_1
             for blocked_col in range(col_1+1, col_2):
+                # This could really be improved...
                 if blocked_col in WAITING_COLS and cur_waitings[WAITING_COLS.index(blocked_col)] != "":
                     return True
             return False
         
+        # Move from a room to a waiting spot.
         for room_idx, room in enumerate(cur_rooms):
-            # find the thing to move
             for room_position, to_move in enumerate(room):
                 if to_move == "":
                     continue
@@ -53,7 +50,6 @@ def do_case(inp: str, sample=False):
                     if is_blocked(waiting_col, ROOM_COLS[room_idx]):
                         continue
 
-                    # have this person move over there
                     new_waitings = list(cur_waitings)
                     new_rooms = list(map(list, cur_rooms))
 
@@ -62,19 +58,16 @@ def do_case(inp: str, sample=False):
                     new_rooms[room_idx][room_position] = ""
                     out.append((cost, (tuple(new_waitings), tuple(map(tuple, new_rooms)))))
         
-        # move from waiting to room
+        # Move from a waiting spot to a room.
         for waiting_idx, waiting_col in enumerate(WAITING_COLS):
-            # find the thing to move
             to_move = cur_waitings[waiting_idx]
             if to_move == "":
                 continue
+
             target_room_idx = ord(to_move) - ord('A')
             target_room = cur_rooms[target_room_idx]
-            # NEED to there
+
             if target_room[0] == "" and all(x == "" or x == to_move for x in target_room[1:]):
-                # move in
-                room_col = ROOM_COLS[target_room_idx]
-                # go back
                 for room_position in range(len(target_room))[::-1]:
                     if target_room[room_position] != "":
                         continue
@@ -83,35 +76,40 @@ def do_case(inp: str, sample=False):
                 else:
                     assert False
 
+                room_col = ROOM_COLS[target_room_idx]
                 if is_blocked(waiting_col, room_col):
                     continue
                 
                 cost = pdist1((room_row, room_col), (WAITING_ROW, waiting_col)) * COST[to_move]
 
                 new_waitings = list(cur_waitings)
-                new_rooms = lmap(list, cur_rooms)
+                new_rooms = list(map(list, cur_rooms))
 
                 new_waitings[waiting_idx] = ""
                 new_rooms[target_room_idx][room_position] = to_move
-                # out.append((cost, (new_waitings, new_rooms)))
                 out.append((cost, (tuple(new_waitings), tuple(map(tuple, new_rooms)))))
 
         return out
     
 
-    # extract words
     rooms = []
     PART2 = ["DD", "CB", "BA", "AC"]
     for i,room_col in enumerate(ROOM_COLS):
         a, b = [lines[row][room_col] for row in [2, 3]]
 
         rooms.append(tuple(a+PART2[i]+b))
+        # Replace the above with the below for part 1.
+        # This also came in handy for testing my generalised part 2 code
+        # to make sure that it works with part 1.
         # rooms.append(tuple(a+b))
     rooms = tuple(rooms)
     print(rooms)
-    waitings = ("",)*len(WAITING_AREAS)
+    waitings = ("",)*len(WAITING_COLS)
 
-    out, _ =a_star((waitings, rooms), expand, FINAL_NODE)
+    # "A*" here is actually "Dijkstra, with a target node".
+    # My internal implementation also returns a path from start to finish,
+    # but I don't use it here.
+    out, _ = a_star((waitings, rooms), expand, FINAL_NODE)
 
     if out:
         print("out:    ", out)
