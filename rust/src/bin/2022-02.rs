a82836af5d18f69623531d0ec3d2969afa5c6202
mcpower <mcpower@users.noreply.github.com>
2022-12-02 23:07:10 +1100

    2022 Day 2 Part 1, but better

--- rust/src/bin/2022-02.rs
+++ rust/src/bin/2022-02.rs
@@ -1,21 +1,5 @@
 use std::{env, fs};
 
-enum Outcome {
-    Loss,
-    Tie,
-    Win,
-}
-
-fn rps(opponent: &char, me: &char) -> Outcome {
-    match (opponent, me) {
-        (x, y) if x == y => Outcome::Tie,
-        ('A', 'B') => Outcome::Win,
-        ('B', 'C') => Outcome::Win,
-        ('C', 'A') => Outcome::Win,
-        _ => Outcome::Loss,
-    }
-}
-
 fn main() {
     let filename = env::args().nth(1).expect("missing filename arg");
     let contents = fs::read_to_string(filename).expect("opening file failed");
@@ -23,32 +7,14 @@ fn main() {
     let matches = contents
         .split_terminator('\n')
         .map(|line| (line.chars().next().unwrap(), line.chars().nth(2).unwrap()))
+        .map(|(opponent, me)| (opponent as i64 - 'A' as i64, me as i64 - 'X' as i64))
         .collect::<Vec<_>>();
 
     let part1 = matches
         .iter()
         .map(|(opponent, me)| {
-            (
-                *opponent,
-                match me {
-                    'X' => 'A',
-                    'Y' => 'B',
-                    'Z' => 'C',
-                    _ => unreachable!(),
-                },
-            )
-        })
-        .map(|(opponent, me)| {
-            (match me {
-                'A' => 1,
-                'B' => 2,
-                'C' => 3,
-                _ => unreachable!(),
-            }) + (match rps(&opponent, &me) {
-                Outcome::Loss => 0,
-                Outcome::Tie => 3,
-                Outcome::Win => 6,
-            })
+            // me - opponent = 0 if tie, 1 win, 2 if loss
+            me + 1 + (me - opponent + 1).rem_euclid(3) * 3
         })
         .sum::<i64>();
 
