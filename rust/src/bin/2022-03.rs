92c4225a9d8bf51bc409e80ec0150531b21687e8
mcpower <mcpower@users.noreply.github.com>
2022-12-03 21:39:10 +1100

    2022 Day 3 Part 2

--- rust/src/bin/2022-03.rs
+++ rust/src/bin/2022-03.rs
@@ -3,9 +3,10 @@ use std::{collections::HashSet, env, fs};
 fn main() {
     let filename = env::args().nth(1).expect("missing filename arg");
     let contents = fs::read_to_string(filename).expect("opening file failed");
-    let lines = contents.lines();
+    let lines = contents.lines().collect::<Vec<_>>();
 
     let part1 = lines
+        .iter()
         .map(|line| {
             let count = line.chars().count();
 
@@ -35,6 +36,37 @@ fn main() {
         })
         .sum::<i64>();
     println!("part 1: {}", part1);
-    let part2 = 0;
+
+    assert_eq!(lines.len() % 3, 0, "lines aren't divisible by 3");
+    let part2 = lines
+        .chunks_exact(3)
+        .map(|lines| {
+            let &[a, b, c] = lines else {unreachable!("chunks_exact didn't give us three things")};
+
+            let a = HashSet::<char>::from_iter(a.chars());
+            let b = HashSet::from_iter(b.chars());
+            let c = HashSet::from_iter(c.chars());
+
+            let intersection = &(&a & &b) & &c;
+            let mut intersection = intersection.iter();
+            let out = intersection
+                .next()
+                .expect("three elves had nothing in common");
+            assert_eq!(
+                intersection.next(),
+                None,
+                "three elves had more than one thing in common"
+            );
+            *out
+        })
+        .map(|common| {
+            (common as i64)
+                + match common {
+                    'a'..='z' => 1 - 'a' as i64,
+                    'A'..='Z' => 27 - 'A' as i64,
+                    _ => unreachable!("common thing was not alphabetical"),
+                }
+        })
+        .sum::<i64>();
     println!("part 2: {}", part2);
 }
