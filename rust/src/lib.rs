52d4b64b777cc76883e4d987f798d80e37a792f4
mcpower <mcpower@users.noreply.github.com>
2022-12-05 19:38:38 +1100

    Clean up path_in_ancestor

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,20 @@ use std::{
 
 pub fn path_in_ancestor(mut cur_path: &Path, file_name: &OsStr) -> Option<PathBuf> {
     loop {
-        let Some(path) = cur_path
+        if let Some(path) = cur_path
             .read_dir()
             .unwrap()
             .map(|result| result.unwrap())
-            .find(|entry| entry.file_name() == file_name) else {
-                if let Some(parent) = cur_path.parent() {
-                    cur_path = parent;
-                    continue;
-                } else {
-                    break None;
-                }
-            };
-        break Some(path.path());
+            .find(|entry| entry.file_name() == file_name)
+        {
+            break Some(path.path());
+        }
+
+        if let Some(parent) = cur_path.parent() {
+            cur_path = parent;
+            continue;
+        }
+
+        break None;
     }
 }
