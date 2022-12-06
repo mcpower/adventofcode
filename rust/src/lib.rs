2a418c7eb7964d4b6be4f8ecf04dbf88c770bf35
mcpower <mcpower@users.noreply.github.com>
2022-12-06 22:50:07 +1100

    Add get_aoc_start
    I might have implemented this a few years ago in my old Rust code...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use chrono::prelude::*;
 use itertools::Itertools;
 
 pub fn path_in_ancestor(mut cur_path: &Path, file_name: &OsStr) -> Option<PathBuf> {
@@ -26,10 +27,25 @@ pub fn path_in_ancestor(mut cur_path: &Path, file_name: &OsStr) -> Option<PathBu
     }
 }
 
+type AocYear = u16;
+type AocDay = u8;
+type AocYearDay = (AocYear, AocDay);
+
 /// this function is cursed
-pub fn get_program_day_and_year() -> Option<(u16, u8)> {
+pub fn get_program_day_and_year() -> Option<AocYearDay> {
     let binary_path = PathBuf::from(env::args_os().next()?);
     let binary_name = binary_path.file_name()?.to_str()?;
     let (year, day) = binary_name.split('-').collect_tuple()?;
     Some((year.parse().ok()?, day.parse().ok()?))
+}
+
+pub fn get_aoc_start(year: AocYear, day: AocDay) -> DateTime<FixedOffset> {
+    FixedOffset::east_opt(-5 * 3600)
+        .unwrap()
+        .from_local_datetime(
+            &NaiveDate::from_ymd_opt(year.into(), 12, day.into())
+                .unwrap()
+                .and_time(Default::default()),
+        )
+        .unwrap()
 }
