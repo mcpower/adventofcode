74800b70c82ddeba05e8bbedf253f6f146ca520e
mcpower <mcpower@users.noreply.github.com>
2022-12-06 22:16:33 +1100

    Add cursed get_program_day_and_year

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,10 @@
 use std::{
+    env,
     ffi::OsStr,
     path::{Path, PathBuf},
 };
+
+use itertools::Itertools;
 
 pub fn path_in_ancestor(mut cur_path: &Path, file_name: &OsStr) -> Option<PathBuf> {
     loop {
@@ -21,4 +24,12 @@ pub fn path_in_ancestor(mut cur_path: &Path, file_name: &OsStr) -> Option<PathBu
 
         break None;
     }
+}
+
+/// this function is cursed
+pub fn get_program_day_and_year() -> Option<(u16, u8)> {
+    let binary_path = PathBuf::from(env::args_os().next()?);
+    let binary_name = binary_path.file_name()?.to_str()?;
+    let (year, day) = binary_name.split('-').collect_tuple()?;
+    Some((year.parse().ok()?, day.parse().ok()?))
 }
