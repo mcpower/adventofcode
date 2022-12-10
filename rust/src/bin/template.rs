730e95866b155cacd7a660e5469b847d50db4a85
mcpower <mcpower@users.noreply.github.com>
2022-12-10 20:04:55 +1100

    Add Rust template

---
+++ rust/src/bin/template.rs
@@ -0,0 +1,25 @@
+use mcpower_aoc::runner::run_samples_and_arg;
+
+fn solve(inp: &str) -> (i64, i64) {
+    let part1 = 0;
+
+    let part2 = 0;
+
+    (part1, part2)
+}
+
+fn main() {
+    run_samples_and_arg(solve, SAMPLES);
+}
+
+const SAMPLES: &[&str] = &[
+    r"
+
+", r"
+
+", r"
+
+", r"
+
+",
+];
