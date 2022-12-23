0a5cba0aceabc190dfcc34315b6d8a89aa5d727e
mcpower <mcpower@users.noreply.github.com>
2022-12-23 16:29:43 +1100

    Fix warnings in template

--- rust/src/bin/template.rs
+++ rust/src/bin/template.rs
@@ -1,6 +1,9 @@
 use mcpower_aoc::runner::run_samples_and_arg;
 
 fn solve(inp: &str, _is_sample: bool) -> (i64, i64) {
+    if _is_sample {
+        dbg!(inp);
+    }
     let part1 = 0;
 
     let part2 = 0;
