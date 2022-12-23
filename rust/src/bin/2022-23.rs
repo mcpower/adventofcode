b7caddbfacd633dd3f4a07fd6ab66437b4fa4a73
mcpower <mcpower@users.noreply.github.com>
2022-12-23 17:14:05 +1100

    2022 Day 23: Refactor Part 1 and 2 together

--- rust/src/bin/2022-23.rs
+++ rust/src/bin/2022-23.rs
@@ -24,106 +24,69 @@ fn solve(inp: &str, _is_sample: bool) -> (i64, i64) {
         (west, [west, west + north, west + south]),
         (east, [east, east + north, east + south]),
     ];
-    let part1 = {
-        let mut positions = initial_positions.clone();
-        for round in 0..10 {
-            let mut proposed = HashMap::<Vec2, Vec2>::new();
-            for position in positions.iter() {
-                if (-1..=1)
-                    .cartesian_product(-1..=1)
-                    .filter(|x| *x != (0, 0))
-                    .map(|(a, b)| Vec2(a, b))
-                    .all(|v| !positions.contains(&(v + *position)))
-                {
-                    continue;
-                }
-                for step in 0..4 {
-                    let (dir, check) = directions[(round + step) % 4];
-                    if check
-                        .iter()
-                        .all(|check_dir| !positions.contains(&(*check_dir + *position)))
-                    {
-                        assert!(proposed.insert(*position, *position + dir).is_none());
-                        break;
-                    }
-                }
+
+    let mut part1 = None;
+    let mut part2 = None;
+
+    let mut positions = initial_positions;
+    let mut round = 0;
+    while part1.is_none() || part2.is_none() {
+        let mut proposed = HashMap::<Vec2, Vec2>::new();
+        let mut moved = false;
+        for position in positions.iter() {
+            if (-1..=1)
+                .cartesian_product(-1..=1)
+                .filter(|x| *x != (0, 0))
+                .map(|(a, b)| Vec2(a, b))
+                .all(|v| !positions.contains(&(v + *position)))
+            {
+                continue;
             }
-            let collisions = proposed
-                .values()
-                .duplicates()
-                .cloned()
-                .collect::<HashSet<_>>();
-            for (start, end) in proposed {
-                if !collisions.contains(&end) {
-                    assert!(positions.remove(&start));
-                    assert!(positions.insert(end));
+            for step in 0..4 {
+                let (dir, check) = directions[(round + step) % 4];
+                if check
+                    .iter()
+                    .all(|check_dir| !positions.contains(&(*check_dir + *position)))
+                {
+                    assert!(proposed.insert(*position, *position + dir).is_none());
+                    break;
                 }
             }
         }
-        let (minx, maxx) = positions
-            .iter()
-            .map(|v| v.0)
-            .minmax()
-            .into_option()
-            .unwrap();
-        let (miny, maxy) = positions
-            .iter()
-            .map(|v| v.1)
-            .minmax()
-            .into_option()
-            .unwrap();
-        // if _is_sample {
-        //     dbg!(&positions);
-        // }
-        (maxy - miny + 1) * (maxx - minx + 1) - (positions.len() as i64)
-    };
-
-    let part2 = {
-        let mut positions = initial_positions;
-        let mut round = 0;
-        loop {
-            let mut proposed = HashMap::<Vec2, Vec2>::new();
-            let mut moved = false;
-            for position in positions.iter() {
-                if (-1..=1)
-                    .cartesian_product(-1..=1)
-                    .filter(|x| *x != (0, 0))
-                    .map(|(a, b)| Vec2(a, b))
-                    .all(|v| !positions.contains(&(v + *position)))
-                {
-                    continue;
-                }
-                for step in 0..4 {
-                    let (dir, check) = directions[(round + step) % 4];
-                    if check
-                        .iter()
-                        .all(|check_dir| !positions.contains(&(*check_dir + *position)))
-                    {
-                        assert!(proposed.insert(*position, *position + dir).is_none());
-                        break;
-                    }
-                }
-            }
-            let collisions = proposed
-                .values()
-                .duplicates()
-                .cloned()
-                .collect::<HashSet<_>>();
-            for (start, end) in proposed {
-                if !collisions.contains(&end) {
-                    assert!(positions.remove(&start));
-                    assert!(positions.insert(end));
-                    moved = true;
-                }
-            }
-            round += 1;
-            if !moved {
-                break round as i64;
+        let collisions = proposed
+            .values()
+            .duplicates()
+            .cloned()
+            .collect::<HashSet<_>>();
+        for (start, end) in proposed {
+            if !collisions.contains(&end) {
+                assert!(positions.remove(&start));
+                assert!(positions.insert(end));
+                moved = true;
             }
         }
-    };
+        round += 1;
+        if round == 10 {
+            let (minx, maxx) = positions
+                .iter()
+                .map(|v| v.0)
+                .minmax()
+                .into_option()
+                .unwrap();
+            let (miny, maxy) = positions
+                .iter()
+                .map(|v| v.1)
+                .minmax()
+                .into_option()
+                .unwrap();
+            part1 = Some((maxy - miny + 1) * (maxx - minx + 1) - (positions.len() as i64));
+        }
+        if !moved && part2.is_none() {
+            part2 = Some(round as i64);
+        }
+    }
 
-    (part1, part2)
+    (part1.unwrap(), part2.unwrap())
 }
 
 fn main() {
