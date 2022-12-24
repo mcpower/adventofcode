d0000217871241553b255ab0da7f7abf844c612d
mcpower <mcpower@users.noreply.github.com>
2022-12-24 20:10:26 +1100

    2022 Day 24 - Improve original solution

--- rust/src/bin/2022-24.rs
+++ rust/src/bin/2022-24.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, mem::swap};
+use std::collections::HashSet;
 
 use mcpower_aoc::{
     runner::run_samples_and_arg,
@@ -35,169 +35,57 @@ fn solve(inp: &str, _is_sample: bool) -> (i64, i64) {
         })
         .collect::<Vec<_>>();
 
-    let part1 = {
-        // "what positions could you be in after every single move?"
-        let mut blizzards = initial_blizzards.clone();
-        let mut out = 0;
+    let start = Vec2(-1, 0);
+    let goal = Vec2(inner_rows, inner_cols - 1);
+
+    let solve = |start: Vec2, start_time: i64, goal: Vec2| {
+        let mut blizzards = initial_blizzards
+            .iter()
+            .map(|&(initial_pos, delta)| {
+                let mut out = initial_pos + (start_time * delta);
+                out.0 = out.0.rem_euclid(inner_rows);
+                out.1 = out.1.rem_euclid(inner_cols);
+                (out, delta)
+            })
+            .collect::<Vec<_>>();
+        let mut out = start_time;
         let mut cur_positions = HashSet::new();
-        cur_positions.insert(Vec2(-1, 0));
-        let target = Vec2(inner_rows, inner_cols - 1);
-        let mut new_cur_positions = HashSet::new();
-        while !cur_positions.contains(&target) {
+        cur_positions.insert(start);
+        while !cur_positions.contains(&goal) {
             for blizzard in &mut blizzards {
                 blizzard.0 += blizzard.1;
                 blizzard.0 .0 = blizzard.0 .0.rem_euclid(inner_rows);
                 blizzard.0 .1 = blizzard.0 .1.rem_euclid(inner_cols);
             }
-            for &position in &cur_positions {
-                for adj in FOUR_ADJ {
-                    let new_position = position + adj;
-                    if new_position == target
-                        || (0 <= new_position.1
-                            && new_position.1 < inner_cols
-                            && 0 <= new_position.0
-                            && new_position.0 < inner_rows)
-                    {
-                        if !blizzards
-                            .iter()
-                            .any(|(blizzard_pos, _)| *blizzard_pos == new_position)
-                        {
-                            new_cur_positions.insert(new_position);
-                        }
-                    }
-                }
-                if !blizzards
-                    .iter()
-                    .any(|(blizzard_pos, _)| *blizzard_pos == position)
-                {
-                    new_cur_positions.insert(position);
-                }
-            }
-            swap(&mut cur_positions, &mut new_cur_positions);
-            new_cur_positions.clear();
+            cur_positions = cur_positions
+                .drain()
+                .flat_map(|pos| {
+                    FOUR_ADJ
+                        .iter()
+                        .map(move |delta| pos + *delta)
+                        .filter(|new_pos| {
+                            *new_pos == goal
+                                || 0 <= new_pos.1
+                                    && new_pos.1 < inner_cols
+                                    && 0 <= new_pos.0
+                                    && new_pos.0 < inner_rows
+                        })
+                        .chain(std::iter::once(pos))
+                        .filter(|new_pos| {
+                            blizzards
+                                .iter()
+                                .all(|(blizzard_pos, _)| *blizzard_pos != *new_pos)
+                        })
+                })
+                .collect();
             out += 1;
         }
         out
     };
 
-    let part2 = {
-        // just run it three times lol
-        let mut blizzards = initial_blizzards;
-        let mut out = 0;
-        let mut cur_positions = HashSet::new();
-        cur_positions.insert(Vec2(-1, 0));
-        let target = Vec2(inner_rows, inner_cols - 1);
-        let mut new_cur_positions = HashSet::new();
-        while !cur_positions.contains(&target) {
-            for blizzard in &mut blizzards {
-                blizzard.0 += blizzard.1;
-                blizzard.0 .0 = blizzard.0 .0.rem_euclid(inner_rows);
-                blizzard.0 .1 = blizzard.0 .1.rem_euclid(inner_cols);
-            }
-            for &position in &cur_positions {
-                for adj in FOUR_ADJ {
-                    let new_position = position + adj;
-                    if new_position == target
-                        || (0 <= new_position.1
-                            && new_position.1 < inner_cols
-                            && 0 <= new_position.0
-                            && new_position.0 < inner_rows)
-                    {
-                        if !blizzards
-                            .iter()
-                            .any(|(blizzard_pos, _)| *blizzard_pos == new_position)
-                        {
-                            new_cur_positions.insert(new_position);
-                        }
-                    }
-                }
-                if !blizzards
-                    .iter()
-                    .any(|(blizzard_pos, _)| *blizzard_pos == position)
-                {
-                    new_cur_positions.insert(position);
-                }
-            }
-            swap(&mut cur_positions, &mut new_cur_positions);
-            new_cur_positions.clear();
-            out += 1;
-        }
-        cur_positions.clear();
-        cur_positions.insert(target);
-        let target = Vec2(-1, 0);
-        while !cur_positions.contains(&target) {
-            for blizzard in &mut blizzards {
-                blizzard.0 += blizzard.1;
-                blizzard.0 .0 = blizzard.0 .0.rem_euclid(inner_rows);
-                blizzard.0 .1 = blizzard.0 .1.rem_euclid(inner_cols);
-            }
-            for &position in &cur_positions {
-                for adj in FOUR_ADJ {
-                    let new_position = position + adj;
-                    if new_position == target
-                        || (0 <= new_position.1
-                            && new_position.1 < inner_cols
-                            && 0 <= new_position.0
-                            && new_position.0 < inner_rows)
-                    {
-                        if !blizzards
-                            .iter()
-                            .any(|(blizzard_pos, _)| *blizzard_pos == new_position)
-                        {
-                            new_cur_positions.insert(new_position);
-                        }
-                    }
-                }
-                if !blizzards
-                    .iter()
-                    .any(|(blizzard_pos, _)| *blizzard_pos == position)
-                {
-                    new_cur_positions.insert(position);
-                }
-            }
-            swap(&mut cur_positions, &mut new_cur_positions);
-            new_cur_positions.clear();
-            out += 1;
-        }
-        cur_positions.clear();
-        cur_positions.insert(target);
-        let target = Vec2(inner_rows, inner_cols - 1);
-        while !cur_positions.contains(&target) {
-            for blizzard in &mut blizzards {
-                blizzard.0 += blizzard.1;
-                blizzard.0 .0 = blizzard.0 .0.rem_euclid(inner_rows);
-                blizzard.0 .1 = blizzard.0 .1.rem_euclid(inner_cols);
-            }
-            for &position in &cur_positions {
-                for adj in FOUR_ADJ {
-                    let new_position = position + adj;
-                    if new_position == target
-                        || (0 <= new_position.1
-                            && new_position.1 < inner_cols
-                            && 0 <= new_position.0
-                            && new_position.0 < inner_rows)
-                    {
-                        if !blizzards
-                            .iter()
-                            .any(|(blizzard_pos, _)| *blizzard_pos == new_position)
-                        {
-                            new_cur_positions.insert(new_position);
-                        }
-                    }
-                }
-                if !blizzards
-                    .iter()
-                    .any(|(blizzard_pos, _)| *blizzard_pos == position)
-                {
-                    new_cur_positions.insert(position);
-                }
-            }
-            swap(&mut cur_positions, &mut new_cur_positions);
-            new_cur_positions.clear();
-            out += 1;
-        }
-        out
-    };
+    let part1 = solve(start, 0, goal);
+    let part1point5 = solve(goal, part1, start);
+    let part2 = solve(start, part1point5, goal);
 
     (part1, part2)
 }
