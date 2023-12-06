abdeb7e0763d5f4630da44c12d1db20dbd635c09
mcpower <mcpower@users.noreply.github.com>
2023-12-06 22:22:31 +1100

    2023 Day 1 Part 2

--- rust/src/bin/2023-01.rs
+++ rust/src/bin/2023-01.rs
@@ -1,20 +1,48 @@
+use aho_corasick::{AhoCorasick, PatternID};
 use mcpower_aoc::runner::run_samples_and_arg;
+
+const NUMBERS: &[&str] = &[
+    "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7",
+    "eight", "8", "nine", "9",
+];
+
+fn pattern_id_to_digit(id: PatternID) -> i64 {
+    (id.as_i32() / 2 + 1).into()
+}
 
 fn solve(
     #[allow(unused_variables)] inp: &str,
     #[allow(unused_variables)] is_sample: bool,
 ) -> (i64, i64) {
+    let ac = AhoCorasick::new(NUMBERS).unwrap();
     let part1 = inp
         .lines()
         .map(|line| {
             let mut nums = line.chars().filter_map(|c| c.to_digit(10));
-            let first = nums.next().expect("line doesn't have digit");
+            let first = nums.next().unwrap_or(0);
+            if !is_sample && first == 0 {
+                panic!("real case did not have any numbers");
+            }
             let last = nums.last().unwrap_or(first);
             i64::from(first * 10 + last)
         })
         .sum();
 
-    let part2 = 0;
+    let part2 = inp
+        .lines()
+        .map(|line| {
+            let mut first = (line.len(), 0);
+            let mut last = (0, 0);
+            for mat in ac.find_overlapping_iter(line) {
+                let cur = (mat.start(), pattern_id_to_digit(mat.pattern()));
+                first = first.min(cur);
+                last = last.max(cur);
+            }
+            assert_ne!(first.1, 0, "couldn't find first??");
+            assert_ne!(last.1, 0, "couldn't find last??");
+            first.1 * 10 + last.1
+        })
+        .sum();
 
     (part1, part2)
 }
@@ -31,7 +59,13 @@ a1b2c3d4e5f
 treb7uchet
 ",
     r"
-
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 ",
     r"
 
